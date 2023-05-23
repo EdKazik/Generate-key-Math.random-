0d8bfbaaa88024b92d8031810defd8e0bab1a90d
EdKazik <edoskazik@gmail.com>
2023-05-23 20:34:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateKey(length, elem) {
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+        const randomPoz = Math.floor(Math.random() * characters.length);
+        randomString += characters.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(4, characters);
+console.log(key);
